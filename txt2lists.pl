54d5b15e98fab5d07503362539009b1f7ae5ce1b
Ahmad Fatoum <ahmad@a3f.at>
2016-10-02 13:54:10 +0200

    3,5x performance increase due to Orcish maneuver and greedy matching
    Tests on Egyptian-Arabic wikipedia (76mb .xml file)
    
    Memoization Non-Greedy   time [s]
                             42.375
                    X        55.412
         X                   11.899
         X          X        24.702

--- txt2lists.pl
+++ txt2lists.pl
@@ -11,9 +11,10 @@ use Lingua::AR::Tashkeel v0.004 'strip';
 use autodie ':all';
 
 local $/; $, = "\n";
-my ($filename, @words, %freq) = $ARGV[0] // 'stdin';
-@words = split ' ', <> =~ s/[\P{InArabic}\p{Punct}\d]+?/ /gurx;
-$freq{strip $_}++ foreach @words;
+my ($filename, @words, %freq, %stripped) = $ARGV[0] // 'stdin';
+@words = split ' ', <> =~ s/[\P{InArabic}\p{Punct}\d]+/ /gurx;
+$freq{$stripped{$_} ||= strip $_}++ foreach @words;
+
 open my $fh, '>', "$filename.freqlist";
 foreach my $word (sort { $freq{$b} <=> $freq{$a} } keys %freq) {
     print $fh "$word $freq{$word}\n";
@@ -21,5 +22,5 @@ foreach my $word (sort { $freq{$b} <=> $freq{$a} } keys %freq) {
 open $fh, '>', "$filename.wordlist";
 print $fh grep { length > 1 } uniq {
     sort => 1,
-    compare => sub { strip(shift) cmp strip(shift) }
+    compare => sub { $stripped{$_[0]} cmp $stripped{$_[1]} }
 }, @words;
