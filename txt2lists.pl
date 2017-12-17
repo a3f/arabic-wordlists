08e9c789f9589a3d1486fdf57de47be77f94f9cf
Ahmad Fatoum <ahmad@a3f.at>
2017-12-17 23:45:45 +0100

    Close file handler when no longer needed

--- txt2lists.pl
+++ txt2lists.pl
@@ -11,14 +11,16 @@ use Lingua::AR::Tashkeel v0.004 'strip';
 use autodie ':all';
 
 local $/; $, = "\n";
-my ($content, $filename, @words, %freq, %stripped) = (<>, $ARGV[0] // 'stdin');
-@words = split ' ', $content =~ s/[\P{InArabic}\p{Punct}\d]+/ /gux;
+my ($content, $filename) = (<>, $ARGV[0] // 'stdin');
+my (@words, %freq, %stripped);
+@words = split ' ', $content =~ s/[\P{InArabic}\p{Punct}\d]+/ /gu;
 $freq{$stripped{$_} ||= strip $_}++ foreach @words;
 
 open my $fh, '>', "$filename.freqlist";
 foreach my $word (sort { $freq{$b} <=> $freq{$a} } keys %freq) {
     print $fh "$word $freq{$word}\n";
 }
+close $fh;
 open $fh, '>', "$filename.wordlist";
 print $fh grep { length > 1 } uniq {
     sort => 1,
